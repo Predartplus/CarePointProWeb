92f09c9029bb0b09bdc6a0b97bd956e24b269690
cva1989 <57489927+cva1989@users.noreply.github.com>
2024-11-05 10:58:38 +0530

    working on the masters

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import LandingPage from './pages/LandingPage';
 import Dashboard from './pages/Dashboard/Dashboard';
 import { useSelector } from 'react-redux';
 import Profile from './pages/Profile';
+import Masters from './pages/Masters/Masters';
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -43,8 +44,9 @@ function App() {
   return loading ? (
     <Loader />
   ) : (
-    <LandingPageLayout>
-      <Routes>
+    <Routes>
+      {/* Landing Page Layout */}
+      <Route element={<LandingPageLayout />}>
         <Route
           index
           element={
@@ -53,7 +55,7 @@ function App() {
               <LandingPage />
             </>
           }
-          />
+        />
         <Route
           path="/signin"
           element={
@@ -72,8 +74,12 @@ function App() {
             </>
           }
         />
-        <Route 
-          path="/dashboard" 
+      </Route>
+
+      {/* Default Layout */}
+      <Route element={<DefaultLayout />}>
+        <Route
+          path="/dashboard"
           element={
             <>
               <PageTitle title="Dashboard | CarePoint Pro" />
@@ -94,121 +100,19 @@ function App() {
             </>
           }
         />
-      </Routes>
-      
-    </LandingPageLayout>
-    // <DefaultLayout>
-    //   <Routes>
-    //     <Route
-    //       index
-    //       element={
-    //         <>
-    //           <PageTitle title="eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-    //           <ECommerce />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/calendar"
-    //       element={
-    //         <>
-    //           <PageTitle title="Calendar | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-    //           <Calendar />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/profile"
-    //       element={
-    //         <>
-    //           <PageTitle title="Profile | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-    //           <Profile />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/forms/form-elements"
-    //       element={
-    //         <>
-    //           <PageTitle title="Form Elements | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-    //           <FormElements />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/forms/form-layout"
-    //       element={
-    //         <>
-    //           <PageTitle title="Form Layout | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-    //           <FormLayout />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/tables"
-    //       element={
-    //         <>
-    //           <PageTitle title="Tables | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-    //           <Tables />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/settings"
-    //       element={
-    //         <>
-    //           <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-    //           <Settings />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/chart"
-    //       element={
-    //         <>
-    //           <PageTitle title="Basic Chart | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-    //           <Chart />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/ui/alerts"
-    //       element={
-    //         <>
-    //           <PageTitle title="Alerts | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-    //           <Alerts />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/ui/buttons"
-    //       element={
-    //         <>
-    //           <PageTitle title="Buttons | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-    //           <Buttons />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/auth/signin"
-    //       element={
-    //         <>
-    //           <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-    //           <SignIn />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/auth/signup"
-    //       element={
-    //         <>
-    //           <PageTitle title="Signup | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-    //           <SignUp />
-    //         </>
-    //       }
-    //     />
-    //   </Routes>
-    // </DefaultLayout>
+        <Route
+          path="/masters"
+          element={
+            <>
+              <PageTitle title="Masters | CarePoint Pro" />
+              <PrivateRoute>
+                <Masters />
+              </PrivateRoute>
+            </>
+          }
+        />
+      </Route> 
+    </Routes>
   );
 }
 
